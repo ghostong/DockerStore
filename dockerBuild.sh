8fcc72bf1f48159f22528d3f89d596fbecc76627
litong <ghostong@126.com>
2019-11-12 18:56:52 +0800

    修改主项目构建方法

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp ./Docker/DockerStore/dockerfile .
-docker build -t docker-store:latest .
+docker build -t registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:docker-store:latest .
 rm ./dockerfile
+docker push registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:latest
--- dockerStart.sh
+++ dockerStart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run -it --rm \
+docker run -itd \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -p 9000:9000 \
     --env USERNAME="ghost" \
     --env PASSWORD="123456" \
     --name DockerStore \
-    docker-store:latest
+    registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:docker-store:latest
