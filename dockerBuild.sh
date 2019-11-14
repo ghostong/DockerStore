25f96b15ce4fb0762872d7a7677a04632486b088
litong <ghostong@126.com>
2019-11-14 16:03:19 +0800

    修改主镜像构建方式

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp ./Docker/DockerStore/dockerfile .
-docker build -t registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:docker-store:latest .
+docker-compose build
 rm ./dockerfile
-docker push registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:latest
+docker-compose push
--- dockerStart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run -itd \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -p 9000:9000 \
-    --env USERNAME="username" \
-    --env PASSWORD="password" \
-    --name DockerStore \
-    registry.cn-hangzhou.aliyuncs.com/litosrc/decker-store:latest
