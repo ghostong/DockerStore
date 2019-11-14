f7b06d5ba3ad2ae53c1e3d98fc22df954c6d311a
litong <ghostong@126.com>
2019-11-14 15:35:32 +0800

    去除NextCloud,SSLBuild

--- App/NextCloud/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ ! -f "/workdir/config/config.php" ]];then
-    touch /workdir/config/CAN_INSTALL
-    chown www-data:www-data /workdir/data
-    chown www-data:www-data /workdir/config
-fi
-chown www-data:www-data /workdir/apps
-/etc/init.d/php7.2-fpm start
-nginx -g 'daemon off;'
