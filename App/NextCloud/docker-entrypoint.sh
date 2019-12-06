4ff7d5f54e7b9dacee05fb659a0efcf3e67a859f
litong <ghostong@126.com>
2019-12-06 18:10:09 +0800

    修改了nextcloud

--- App/NextCloud/docker-entrypoint.sh
+++ App/NextCloud/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ ! -f "/workdir/config/CAN_INSTALL" ]]; then
+if [[ -f "/workdir/config/CAN_INSTALL" ]]; then
     unzip -qo -d /workdir/ /tmp/dependence/nextcloud-17.0.1.zip && mv /workdir/nextcloud/* /workdir && rm -rf /workdir/nextcloud
     mkdir /workdir/data
     chown  -R www-data /workdir/config /workdir/data /workdir/apps
 fi
 
-if [[ ! -f "/etc/nginx/ssl/nexccloud.crt" ]]; then
+if [[ ! -f "/etc/nginx/ssl/nextcloud.crt" ]]; then
     cp -r /tmp/dependence/ssl/* /etc/nginx/ssl/
 fi
 
