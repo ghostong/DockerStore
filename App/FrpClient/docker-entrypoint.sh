11469450ea4e015b04ba77da49b08ab303238f87
litong <ghostong@126.com>
2019-12-07 17:58:50 +0800

    增加frps,防止frpc启动问题

--- App/FrpClient/docker-entrypoint.sh
+++ App/FrpClient/docker-entrypoint.sh
@@ -3,9 +3,13 @@
 cp -r /tmp/dependence/frpc /workdir
 cp /tmp/dependence/frpc_example.ini /volume/config/frpc_example.ini
 
+cp -r /tmp/dependence/frps /workdir
+cp /tmp/dependence/frps.ini /workdir/frps.ini
 
 if [[ ! -e /volume/config/frpc.ini ]]; then
     cp /tmp/dependence/frpc.ini /volume/config/frpc.ini
 fi
+
+./frps -c /workdir/frps.ini &
 
 exec "$@"
