f7057db855226d12d622e225456e912a92dd6633
litong <ghostong@126.com>
2019-12-07 10:15:45 +0800

    修改nextcloud配置

--- App/NextCloud/docker-entrypoint.sh
+++ App/NextCloud/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [[ -f "/workdir/config/CAN_INSTALL" ]]; then
+cp -rf /tmp/dependence/fpm/* /etc/php/7.2/fpm
+
+ln -s /etc/php/7.2/mods-available /etc/php/7.2/fpm/conf.d
+
+if [[ ! -f "/workdir/config/config.php" ]]; then
     unzip -qo -d /workdir/ /tmp/dependence/nextcloud-17.0.1.zip && mv /workdir/nextcloud/* /workdir && rm -rf /workdir/nextcloud
     mkdir /workdir/data
     chown  -R www-data /workdir/config /workdir/data /workdir/apps
