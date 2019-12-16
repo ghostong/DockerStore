24c85f24361159406856278a2c8feff00ae7c07a
litong <ghostong@126.com>
2019-12-16 19:02:29 +0800

    修改验证方式,增加http身份认证

--- App/Aria2/docker-entrypoint.sh
+++ App/Aria2/docker-entrypoint.sh
@@ -4,6 +4,10 @@ if [[ ! -e /etc/nginx/sites-available/default ]]; then
    cp /tmp/dependence/default /etc/nginx/sites-available/default
 fi
 
+if [[ ! -e /volume/config/htpasswd ]]; then
+     htpasswd -bc /volume/config/htpasswd admin 123@456@
+fi
+
 if [[ ! -e /volume/config/aria2.conf ]]; then
     cp /tmp/dependence/aria2.conf /volume/config/aria2.conf
 fi
