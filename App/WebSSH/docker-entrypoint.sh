73c3d106006295d82be71c83a1cd8a904f718294
litong <ghostong@126.com>
2020-03-28 18:51:56 +0800

    增加webssh config

--- App/WebSSH/docker-entrypoint.sh
+++ App/WebSSH/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ ! -f "/root/.ssh/config" ]]; then
+   cp /tmp/config /root/.ssh/config
+   chmod 700 /root/.ssh
+   chmod 600 /root/.ssh/config
+fi
+
 /etc/init.d/ssh start
 
 exec "$@"
