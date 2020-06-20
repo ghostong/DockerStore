dbb6ca84977304f1b47f3226d18cb279891bcb25
litong <ghostong@126.com>
2020-06-20 16:38:07 +0800

    修改第一次启动bug

--- App/MinIO/docker-entrypoint.sh
+++ App/MinIO/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /volume/config/certs/
 cp -r /etc/nginx/ssl/dockerstore.ssh2.cc.key /volume/config/certs/private.key
 cp -r /etc/nginx/ssl/dockerstore.ssh2.cc.pem /volume/config/certs/public.crt
 exec $@
