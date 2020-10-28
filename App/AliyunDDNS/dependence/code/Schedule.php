8ceb1b48651591b11710a70d616313d2feb19df4
litong <ghostong@126.com>
2020-10-28 17:06:36 +0800

    debug

--- App/AliyunDDNS/dependence/code/Schedule.php
+++ App/AliyunDDNS/dependence/code/Schedule.php
@@ -1,6 +1,5 @@
 <?php
 $config = (new ConfigModel())->getConfig();
-var_dump($config);
 Lit\Ms\LitMsSchedule::every( $config['interval'] ? : 600, function (){
     (new ScheduleModel())->UpdateDomain();
 },"UpdateDomain Every ". ($config["interval"] ? : 600) ." s");
